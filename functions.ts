1cc0b4df59e9c803731daaaa475559a58d5d0ff3
cambrown101 <cameron-brown@live.com>
2021-02-03 14:51:35 -0800

    Default function params and optional params

--- functions.ts
+++ functions.ts
@@ -6,9 +6,22 @@ const sayHello = () => {
 
 sayHello();
 
-const sayWord = (word: string): string => {
-  console.log(word);
+// ? for optional params
+// const sayWord = (word?: string): string => {
+//   console.log(word || 'hello');
+//   return word || 'hello';
+// };
+
+// sayWord();
+
+//default params
+const sayWord = (word = 'Hello', ...otherStuff: string[]): string => {
+  console.log(otherStuff);
   return word;
 };
 
-sayWord('Cam');
+sayWord('Cam', 'Scott');
+
+let newName = 'Cam';
+newName = 'Scott';
+newName = 10;
